7c98d27a28ad509912f3e21725bb69807177ae76
Gurvinder <gurvinder.singh@chd.trantorinc.com>
2017-01-04 16:50:31 +0530

    edit file

--- add.c
+++ add.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<conio.h>
 int add(int x,int y)
 {
 	int z;
@@ -7,13 +7,18 @@ int add(int x,int y)
 	return z;	
 
 }
-
+int mul(int x1,int y1)
+{
+return x1*y1;
+}
 void main()
 {
-	int a,b,c;
+	int a,b,c,result;
 	scanf("%d%d",&a,&b);
 	
 	c=add(a,b);
 	printf("Sum of the two no.s is %d",c);
+	result=mul(a,b);
+	printf("mul of the two no.s is %d",result);
 
 }
