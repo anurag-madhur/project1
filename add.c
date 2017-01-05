93af9fff3c9f74d37b949c9b173b51f14f07e986
Tushar <tushar.gupta@chd.trantorinc.com>
2017-01-05 12:45:09 +0530

    include multiplication feature

--- add.c
+++ add.c
@@ -8,13 +8,22 @@ int add(int x,int y)
 
 }
 
+int mul(int x,int y)
+{
+	int z;
+	z=x*y;
+	return z;	
+
+}
+
 void main()
 {
 	int a,b,c,result;
 	scanf("%d%d",&a,&b);
 	
 	c=add(a,b);
+	result=mul(a,b);
 	printf("Sum of the two no.s is %d",c);
-	
+	printf("mul of the two no.s is %d",result);
 
 }
