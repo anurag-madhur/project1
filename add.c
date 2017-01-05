7bfbd96f704996693a35fd9cbabb160e6d7e0fc6
Anurag Madhur <anurag.madhur@chd.trantorinc.com>
2017-01-05 12:57:28 +0530

    swap two no

--- add.c
+++ add.c
@@ -1,24 +1,20 @@
 #include<stdio.h>
-#include<conio.h>
-int add(int x,int y)
+
+int swap(int x,int y)
 {
-	int z;
-	z=x+y;
-	return z;	
+	
+	x=x+y;
+        y=x-y;
+        x=x-y;	
 
 }
-int mul(int x1,int y1)
-{
-return x1*y1;
-}
+
 void main()
 {
-	int a,b,c,result;
+	int a,b;
 	scanf("%d%d",&a,&b);
 	
-	c=add(a,b);
-	printf("Sum of the two no.s is %d",c);
-	result=mul(a,b);
-	printf("mul of the two no.s is %d",result);
+	swap(a,b);
+	return 0;
 
 }
